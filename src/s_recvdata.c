7cac2561622e706c41105e7e81f0f2518619db59
Cornelis Terblanche <1997corry@gmail.com>
2020-04-29 19:40:18 +0200

    fixed the bytes beeing wrong

--- src/s_recvdata.c
+++ src/s_recvdata.c
@@ -4,7 +4,7 @@ int	s_recvdata(int fd, fd_set *clients, char *buf){
 	int nbytes;
 
 	//client exists and sent data
-	if ((nbytes = recv(fd, buf, sizeof(buf), 0)) <= 0){
+	if ((nbytes = recv(fd, buf, BUFFER, 0)) <= 0){
 		//error or client closed connection.
 		if (nbytes == 0){
 			//connection closed
