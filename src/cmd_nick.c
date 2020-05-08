b8e8d6e8ae78ce4a151ae16836178ce3e9e62b1b
Cornelis Terblanche <1997corry@gmail.com>
2020-05-08 16:24:22 +0200

    nick changed broadcast

--- src/cmd_nick.c
+++ src/cmd_nick.c
@@ -1,6 +1,10 @@
 #include "../inc/private_irc.h"
 
 void cmd_nick(t_env *e, t_client *client, char *nick){
+    t_client    *run;
+    char        *tmp;
+    char        *tmp2;
+
     assert(e && client && nick);
     nick[ft_strlen(nick) - 1] = '\0';
     if (!(ft_strlen(nick) > 0)){
@@ -10,7 +14,20 @@ void cmd_nick(t_env *e, t_client *client, char *nick){
     } else if (ft_strlen(nick) > NICK_LENGTH) {
         s_status_send(client, "Nick too long, please try another.\n");
     } else {
-        ft_printf("%s changed nick to %s\n", client->nick, nick);
+        run = e->clients;
+        tmp2 = tmp;
+        tmp = ft_strjoin(client->nick, " changed their nick to ");
+        tmp2 = ft_strjoin(tmp, nick);
+        ft_strdel(&tmp);
+        tmp = ft_strjoin(tmp2, "\n");
+        ft_strdel(&tmp2);
+        while (run){
+            if (run->channel == client->channel && run != client){
+                s_status_send(run, tmp);
+            }
+            run = run->next;
+        }
+        ft_strdel(&tmp);
         ft_strcpy(client->nick, nick);
         s_status_send(client, "Nick successfully changed.\n");
     }
