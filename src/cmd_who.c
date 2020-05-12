644d3902ab15292f05b47543a20b7a442250a912
Cornelis Terblanche <1997corry@gmail.com>
2020-05-13 01:48:04 +0200

    fixed double free in cmd_who

--- src/cmd_who.c
+++ src/cmd_who.c
@@ -4,26 +4,29 @@ void cmd_who(t_env_s *e, t_client *client){
     t_client    *run;
     char        *msg;
     char        *tmp;
-    int         len;
 
     assert(e && client);
     run = e->clients;
+    msg = ft_strnew(1);
     while (run){
         if (run->channel == client->channel){
-            tmp = msg;
-            if (ft_strlen(msg) > 0){
-                msg = ft_strjoin(msg, ", ");
-                ft_strdel(&tmp);
+            if (ft_strlen(msg) < 1){
+                msg = ft_strjoin(msg, run->nick);
             }
+            tmp = msg;
+            msg = ft_strjoin(msg, ", ");
+            ft_strdel(&tmp);
+            tmp = msg;
             msg = ft_strjoin(msg, run->nick);
             ft_strdel(&tmp);
         }
         run = run->next;
     }
-    if ((len = ft_strlen(msg)) > 0){
-        tmp = ft_strjoin(msg, "\n");
-        ft_strdel(&msg);
-        s_status_send(client, tmp);
+    if ((ft_strlen(msg)) > 0){
+        tmp = msg;
+        msg = ft_strjoin(msg, "\n");
         ft_strdel(&tmp);
+        s_status_send(client, msg);
+        ft_strdel(&msg);
     }
 }
